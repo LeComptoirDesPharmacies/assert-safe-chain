ed103d0fec96a2e8ecca10f96e136ddf0476dd4b
Antoine <antoine@lecomptoirdespharmacies.fr>
2025-12-05 16:45:09 +0100

    Update version
    Switch from sh to mjs

---
+++ bin/noop.mjs
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+console.log("NOOP in assert safe chain. If safe-chain is active, we should never reach this script");
+process.exit(0);
--- bin/noop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "NOOP in assert safe chain. If safe-chain is active, we should never reach this script"
-
-exit 0
